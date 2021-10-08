f0929c2d727ee05d584dc783f203d255986c78bf
Kraev Anatoly <rjiynbiukot@mail.ru>
2021-10-08 17:47:55 +0300

    add truncatewords and paginator

--- posts/models.py
+++ posts/models.py
@@ -18,7 +18,7 @@ class Group(models.Model):
 
 class Post(models.Model):
     text = models.TextField("Текст записи")
-    pub_date = models.DateTimeField("дата публикации", auto_now_add=True)
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True, related_name='posts')
 
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post, Group
 
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    # latest = Post.objects.order_by("-pub_date")[:11]
+    # return render(request, "index.html", {"posts": latest})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 10)  # По 10 статьи на каждой странице.
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом, возвращаем первую страницу.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'index.html', {'page': page, 'posts': posts})
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
